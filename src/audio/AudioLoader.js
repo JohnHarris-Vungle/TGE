44a35c76667f440a4dac9c93258615e77dafaeeb
John Harris <jharris@tresensa.com>
2020-07-24 08:40:42 -0700

    PAN-1516 Switch audio loading from XMLHttpRequest to fetch

--- src/audio/AudioLoader.js
+++ src/audio/AudioLoader.js
@@ -47,37 +47,27 @@ TGE.WebAudioLoader = function(id, url, url2)
             that.loader.onError(that);
         };
 
-        // Are audio assets packaged?
-        if (window._TREAUDIO)
-        {
-            var parts = this.url.split("/");
-            var filename = parts[parts.length-1];
+		var url = this.url;
 
-	        var base64Str = _TREAUDIO[filename];
+		// Are audio assets packaged?
+		if (window._TREAUDIO)
+		{
+			var parts = url.split("/");
+			var filename = parts[parts.length - 1];
 
-            fetch(base64Str)
-                .then(function(response) {
-                    return response.arrayBuffer();
-                })
-                .then(function(arrayBuffer) {
-                    TGE.AudioManager.GetInstance()._mPlugin._mContext.decodeAudioData(arrayBuffer, onDecodeSuccess, onDecodeError);
-                })
-                .catch(function(error) {
-                    onDecodeError();
-                });
-        }
-        else
-        {
-            var request = new XMLHttpRequest();
-            request.open('GET', this.url, true);
-            request.responseType = 'arraybuffer';
+			url = _TREAUDIO[filename];
+		}
 
-            // Decode asynchronously
-            request.onload = function() {
-                TGE.AudioManager.GetInstance()._mPlugin._mContext.decodeAudioData(request.response, onDecodeSuccess, onDecodeError);
-            };
-            request.send();
-        }
+		fetch(url)
+			.then(function(response) {
+				return response.arrayBuffer();
+			})
+			.then(function(arrayBuffer) {
+				TGE.AudioManager.GetInstance()._mPlugin._mContext.decodeAudioData(arrayBuffer, onDecodeSuccess, onDecodeError);
+			})
+			.catch(function(error) {
+				onDecodeError();
+			});
 	};
 
 	/** @ignore */
